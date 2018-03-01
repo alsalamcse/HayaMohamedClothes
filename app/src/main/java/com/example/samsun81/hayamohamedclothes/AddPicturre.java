3794ceecf238fb83c8b305e812ea15b63677ee2f
user <muhammed.awad@outlook.com>
2018-03-01 12:11:53 +0200

    add picture needs to be related to the firebase to do the auth

--- app/src/main/java/com/example/samsun81/hayamohamedclothes/AddPicturre.java
+++ app/src/main/java/com/example/samsun81/hayamohamedclothes/AddPicturre.java
@@ -7,22 +7,44 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class AddPicturre extends AppCompatActivity {
 
      Button BtTake;
     ImageView imageView;
     private Button btAdd;
+    private EditText etName;
+    private TextView tvTime;
+    private ImageButton ibWinter,ibAutumn,ibSpring,ibSummer,ibSwitch,ibSwitch1,ibSwitch2;
+    private TextView tvOc;
+    private ImageButton ibSad,ibJoy,ibTrip,ibOther;
     private  static  final int CAN_REQUEST=1313;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_picturre);
-        btAdd=(Button)findViewById(R.id.btAdd);
         BtTake=(Button) findViewById(R.id.BtButton);
         imageView=(ImageView)findViewById(R.id.imageView);
+        btAdd=(Button) findViewById(R.id.btAdd);
+        etName=(EditText) findViewById(R.id.etName);
+        tvTime=(TextView) findViewById(R.id.tvTime);
+        ibWinter=(ImageButton) findViewById(R.id.ibWinter);
+        ibAutumn=(ImageButton) findViewById(R.id.ibAutumn);
+        ibSpring=(ImageButton) findViewById(R.id.ibSpring);
+        ibSummer=(ImageButton) findViewById(R.id.ibSummer);
+        ibSwitch=(ImageButton) findViewById(R.id.ibSwitch);
+        ibSwitch1=(ImageButton) findViewById(R.id.ibSwitch1);
+        ibSwitch2=(ImageButton) findViewById(R.id.ibSwitch2);
+        tvOc=(TextView) findViewById(R.id.tvOc);
+        ibSad=(ImageButton) findViewById(R.id.ibSad);
+        ibJoy=(ImageButton) findViewById(R.id.ibJoy);
+        ibTrip=(ImageButton) findViewById(R.id.ibTrip);
+        ibOther=(ImageButton) findViewById(R.id.ibOther);
         BtTake.setOnClickListener(new btnTakePhotoClicker());
     }
 
